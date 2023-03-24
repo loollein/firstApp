7a4aaceb13cf48dd9168901735239028447003f7
Danil <74613455+loollein@users.noreply.github.com>
2023-03-24 10:12:31 +0700

    [Com02] Nesting cycles.

--- script/script.js
+++ script/script.js
@@ -1,17 +1,30 @@
-let numberOfFilms = prompt('Сколько фильмов Вы уже посмотрели?', '');
+// let numberOfFilms = prompt('Сколько фильмов Вы уже посмотрели?', '');
+//
+// let personalMovieBD = {
+//     count: (numberOfFilms),
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     private: false
+// };
+//
+// let lastOfFilmsOne = prompt('Один из просмотренных фильмов?', '');
+// let lastOfFilmsTwo = prompt('Один из просмотренных фильмов?', '');
+// let scoreOfFilmsOne = prompt('Насколько оцените его?', '');
+// let scoreOfFilmsTwo = prompt('Насколько оцените его?', '');
+//
+// personalMovieBD.movies[lastOfFilmsOne] = scoreOfFilmsOne;
+// personalMovieBD.movies[lastOfFilmsTwo] = scoreOfFilmsTwo;
 
-let personalMovieBD = {
-    count: (numberOfFilms),
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false
-};
+let result = '';
+const length = 7;
 
-let lastOfFilmsOne = prompt('Один из просмотренных фильмов?', '');
-let lastOfFilmsTwo = prompt('Один из просмотренных фильмов?', '');
-let scoreOfFilmsOne = prompt('Насколько оцените его?', '');
-let scoreOfFilmsTwo = prompt('Насколько оцените его?', '');
+for (let i = 1; i < length; i++) {
+    for (let j = 0; j < i; j++) {
+        result += '*';
+    }
 
-personalMovieBD.movies[lastOfFilmsOne] = scoreOfFilmsOne;
-personalMovieBD.movies[lastOfFilmsTwo] = scoreOfFilmsTwo;
+    result += '\n';
+}
+
+console.log(result);
